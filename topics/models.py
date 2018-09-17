eba027f7dc17953d2e9585e151c42ba3007b7761
SimonContreras <simon.contreras.13@sansano.usm.cl>
2018-09-17 19:30:52 -0300

    Added new models

--- topics/models.py
+++ topics/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
 class LdaModel(models.Model):
     filename = models.CharField(unique=True, max_length=1000)
     creation_date = models.DateField(auto_now=True)
     newest = models.BooleanField(default=True, null=False)
     in_use = models.BooleanField(default=False, null=False)
-    
+
+
 class Topic(models.Model):
     topic_number = models.IntegerField(null=False)
     lda_model = models.ForeignKey(LdaModel, on_delete=models.CASCADE, related_name='topic_ldamodel')
     name = models.CharField(null=True, blank=True, max_length=100)
+
 
 class Keyword(models.Model):
     name = models.CharField(null=False, max_length=100)
@@ -18,10 +20,19 @@ class Keyword(models.Model):
     topic_id = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='keyword_topic')
 
 
-class TopicComparison(models.Model):
-	topic1_id = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='topic1_topic2')
-	topic2_id = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='topic2_topic1')
-	distance = models.FloatField()
+class TopicUser(models.Model):
+    user_id = models.IntegerField(null=False)
+    topic_id = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='user_topic')
 
-	class Meta:
-		unique_together = ('topic1_id', 'topic2_id')
+    class Meta:
+        unique_together = ("user_id", "topic_id")
+
+
+class TopicComparison(models.Model):
+    topic1_id = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='topic1_topic2')
+    topic2_id = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='topic2_topic1')
+    distance = models.FloatField()
+
+    class Meta:
+        unique_together = ('topic1_id', 'topic2_id')
+
