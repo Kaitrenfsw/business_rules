c2c71dc15a47e1ae4c9a9050b0dab96dcea78262
SimonContreras <simon.contreras.13@sansano.usm.cl>
2018-10-14 13:57:10 -0300

    Update user dashboard preference endpoint

--- user_preferences/views.py
+++ user_preferences/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from .models import Source, TopicUser, Topic, DashboardUser
+from .models import Source, TopicUser, Topic, DashboardUser, UserGraph, TopicGraph
 from .serializers import SourceSerializer, DashboardUserSerializer
 from topics.serializers import TopicKeywordSerializer
 
@@ -163,13 +163,47 @@ class DashboardUserViewSet(viewsets.ViewSet):
 
     @staticmethod
     def update(request, pk=None):
-        return Response(data={":)"})
+        # Response setup
+        response_status = status.HTTP_200_OK
+        response_json = {"User preferences updated!"}
+        try:
+            if 'graphs_selected' in request.data:
+                # Delete older preferences
+                user_preferences = UserGraph.objects.filter(user_id=pk)
+                user_preferences.delete()
+
+                # Save new preferences
+                new_preferences = request.data
+                for graph_preference in new_preferences['graphs_selected']:
+                    dashboard_user_instance = DashboardUser.objects.get(user_id=pk)
+                    new_user_graph = UserGraph(user_id=dashboard_user_instance,
+                                               graph_type_id=graph_preference['graph_type'])
+                    new_user_graph.save()
+                    for topic_selected in graph_preference['topics_selected']:
+                        topic_instance = Topic.objects.get(id=topic_selected['topic_id'])
+                        new_topic_selected = TopicGraph(user_graph=new_user_graph,
+                                                        topic_id=topic_instance)
+                        new_topic_selected.save()
+        except Exception as e:
+            response_json = {"Exception raised": e}
+            response_status = status.HTTP_500_INTERNAL_SERVER_ERROR
+
+        return Response(data=response_json, status=response_status)
 
     @staticmethod
     def partial_update(request):
         return Response(data={":)"})
 
     @staticmethod
-    def destroy(request):
-        return Response(data={":)"})
+    def destroy(request, pk=None):
+        try:
+            user_preferences = UserGraph.objects.filter(user_id=pk)
+            user_preferences.delete()
+            response_status = status.HTTP_200_OK
+            response_json = {"User preferences deleted!"}
+        except Exception as e:
+            response_json = {"Exception raised": e}
+            response_status = status.HTTP_500_INTERNAL_SERVER_ERROR
+
+        return Response(data=response_json, status=response_status)
 
